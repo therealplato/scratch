7d03f82162bd8f41e5ed09428b011de550f9d925
Isaac Rogers <therealplato@gmail.com>
2018-05-15 15:27:17 +1200

    cleanup, gitignore

--- encodings/main.go
+++ encodings/main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"encoding/base64"
+	"encoding/hex"
 	"fmt"
 )
 
 func main() {
 	bb := []byte("hi")
+
 	fmt.Println(base64.URLEncoding.EncodeToString(bb))
+
+	fmt.Println(hex.EncodeToString(bb))
 }
